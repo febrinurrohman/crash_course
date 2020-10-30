a9fad8a03542157b43f58a0fbed4a8ae599a2841
febrinurrohman <vanderfarrel@gmail.com>
2020-10-30 19:19:44 +0700

    chapter 3 - create bicycles.py

---
+++ ch3/bicycles.py
@@ -0,0 +1,6 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}"
+
+print(bicycles[0].title())
+print(bicycles[-2].title())
+print(message)
