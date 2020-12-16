b2fc9a3234c5a7f553efa4d5c4bbde4d295c00d8
febrinurrohman <vanderfarrel@gmail.com>
2020-12-16 21:04:39 +0700

    chapter 3 - modify motorcycles.py

--- ch3/motorcycles.py
+++ ch3/motorcycles.py
@@ -15,8 +15,23 @@
 # print(motorcycles)
 
 
-motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
 print(motorcycles)
 
-del motorcycles[1]
+# del motorcycles[1]
+# print(motorcycles)
+
+# popped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
+# first_owned = motorcycles.pop(0)
+# print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+# motorcycles.remove('ducati')
+# print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
 print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
