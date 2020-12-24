0fa8532f6f2396e48ce9e8a83a8a872e01b69892
febrinurrohman <vanderfarrel@gmail.com>
2020-12-24 11:39:09 +0700

    Chapter 4 - create tuple

---
+++ ch4/tuple/try_it/buffet.py
@@ -0,0 +1,15 @@
+foods = ('nasi', 'sayur', 'tempe', 'semangka', 'teh')
+
+print("Original menu:")
+
+for food in foods:
+    print(food)
+
+# foods[2] = 'ayam'
+
+foods = ('kupat', 'opor', 'kerupuk', 'jeruk', 'sirup')
+
+print("\nNew menu:")
+
+for food in foods:
+    print(food)
